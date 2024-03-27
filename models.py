02633183b9f8d951d79eb7f8e05c501b61793636
Nillson Silva <53544840+Kombait98@users.noreply.github.com>
2024-03-27 16:21:09 -0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id=Column(Integer,primary_key=True,index=True)
+    username=Column(String(50),unique=True)
+    
+class post(Base):
+    __tablename__= 'posts'
+    id=Column(Integer,primary_key=True,index=True)
+    title = Column(String(50))
+    content  = Column(String(100))
+    user_id=Column(Integer)
+    
